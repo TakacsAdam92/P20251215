cee99e6b4e2dc3be2a8152fe541fd8cc47c65ab5
Zaza343 <fzalan2007@gmail.com>
2025-12-15 12:56:26 +0100

    Add project files.

---
+++ P20251215/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace P20251215
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
